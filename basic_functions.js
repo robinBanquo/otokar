f65f192e652ad9c64c80f017cab7232191368da7
robinBanquo <caramaschi@hotmail.com>
2017-06-27 12:03:09 +0200

    merge avec le json

--- basic_functions.js
+++ basic_functions.js
@@ -2,6 +2,13 @@
  * Created by banquo on 27/06/17.
  */
 $(document).ready(function () {
-    $.getJSON('')
-})
-
+    $('#submit').click(function() {
+        var start = $('#start').val();
+        var finish = $('#finish').val();
+        $.getJSON('data/Mende-Clermont_Ferrand.json', function () {
+            $.each(data , function ( stand_name ,  shedule_array) {
+                
+            }
+        });
+    });
+});
