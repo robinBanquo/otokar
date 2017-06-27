fcd32393020b3c4dea857860e5cc0f7ac83e655d
robinBanquo <caramaschi@hotmail.com>
2017-06-27 14:03:16 +0200

    ca continue

--- basic_functions.js
+++ basic_functions.js
@@ -5,17 +5,28 @@ $(document).ready(function () {
     $('#submit').click(function() {
         var departure = $('#departure').val();
         var arrival = $('#arrival').val();
-        var now = new Date();
+        let now = new Date();
+        var appendable_text = "<h1>Heure de départ des trois prochains bus à destination de " + arrival + " : </h1>" +
+            "<ul>";
         $.getJSON('https://raw.githubusercontent.com/robinBanquo/otokar/master/data/Mende-Clermont_Ferrand.json', function (data) {
-            var shedule_array = data[departure] ;
-            shedule_array.each(function (shedule) {
-                if(shedule.substr(0,2) == now.getHours()){
-                    if(shedule.substr(0,2) > now.getHours());
-                    
-                }else if (shedule.substr(0,2) > now.getHours()){
 
+            var schedule_array = data[departure] ;
+            var counter = 0 ;
+            schedule_array.each(function (shedule) {
+                if (counter < 3 ){
+                    if(shedule.substr(0,2) == now.getHours()){
+                        if(shedule.substr(3,2) > now.getMinutes()) {
+                            appendable_text += "<li>" + shedule + "</li> ";
+                            counter++;
+                        }
+                    }else if (shedule.substr(0,2) > now.getHours()){
+                        appendable_text += "<li>" + shedule + "</li> ";
+                        counter++;
+                    }
                 }
-            })
+            });
+            appendable_text += "</ul>";
+
         });
     });
 });
