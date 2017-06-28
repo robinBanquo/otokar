336d188bc641305aef6d319f952b67c06b90de14
Hermeline <bibouille66@gmail.com>
2017-06-28 14:44:25 +0200

    progressive web app

--- progressive_web_app.js
+++ progressive_web_app.js
@@ -1,3 +1,36 @@
-/**
- * Created by banquo on 27/06/17.
- */
+var cacheName = 'pwa'; //le nom de mon cache !
+
+// J'assigne un écouteur 'install' à mon service worker
+self.addEventListener('install', function(e){
+  // Cool il est installé !
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    // Je récupére mon cache du nom de pwa
+    caches.open(cacheName).then(function(cache) {
+      // Je fais là mise en cache de ma PWA
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+      // filesToCache : tableaux contenant les différents path vers les fichiers que je veux stocker.
+    })
+  );
+});
+
+var filesToCache = [
+ '/',
+ 'index.html',
+ 'assets/css/style.css',
+ 'assets/img/bg.jpg',
+ 'assets/img/grille.png',
+]
+
+// J'assigne un écouteur 'fetch' à mon service worker
+self.addEventListener('fetch', function(e) {
+  // J'intercepte bien une requéte x ou y.
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    // Jy réponds soit avec une ressource trouvé dans mon cache (responde) soit en renvoyant la requête
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
